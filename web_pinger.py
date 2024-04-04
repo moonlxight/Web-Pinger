1f2e093aa53dd3bca5f645d56b96b6d0c73f26ee
moonlxight <furkandincer444@gmail.com>
2024-04-04 18:52:47 +0300

    Update web_pinger.py

--- web_pinger.py
+++ web_pinger.py
@@ -1,16 +1,24 @@
-import os  # Sistemle ilgili işlemler için
-import sys  # Sistemle ilgili işlemler için
+# >>=========================================================<<
+# ||  ______         _      __            __               __||
+# || /_  __/      __(_)____/ /___  __    / /__  ____ _____/ /||
+# ||  / / | | /| / / / ___/ __/ / / /_  / / _ \/ __ `/ __  / ||
+# || / /  | |/ |/ / (__  ) /_/ /_/ / /_/ /  __/ /_/ / /_/ /  ||
+# ||/_/   |__/|__/_/____/\__/\__, /\____/\___/\__,_/\__,_/   ||
+# ||                        /____/                           ||
+# >>=========================================================<<
+#           Tʜɪꜱ ᴘʀᴏᴊᴇᴄᴛ ᴡᴀꜱ ᴅᴇᴠᴇʟᴏᴘᴇᴅ ʙʏ TᴡɪꜱᴛʏJᴇᴀᴅ ツ
+#                    ɢɪᴛʜᴜʙ.ᴄᴏᴍ/ᴍᴏᴏɴʟxɪɢʜᴛ
+
+import os
+import sys
 import subprocess  # Harici işlemler çağırmak için
 import socket  # Ağ işlemleri için
-import colorama  # Renkli metinler için kütüphane
-from colorama import Fore, Style, init  # Renkli metinler için
+import colorama
+from colorama import Fore, Style, init
 
-# Renkli metinler için colorama'nın başlatılması
 init()
-
-# Web pinger başlığı için ASCII sanatını çizen fonksiyon
+os.system("title TwistyJead's Web Pinging Tool")
 def web_pinger_art():
-    # ASCII sanatı çizimi
     sys.stderr.writelines(Fore.LIGHTCYAN_EX + """
                                      _       __     __    ____  _                      
                                     | |     / /__  / /_  / __ \(_)___  ____ ____  _____
@@ -19,19 +27,15 @@ def web_pinger_art():
                                     |__/|__/\___/_.___/_/   /_/_/ /_/\__, /\___/_/     
                                                                     /____/             
 """)
-
-# Ana menüyü gösteren fonksiyon
 def menu():
     # Ekran temizleniyor
-    if os.name == "nt":  # Windows işletim sistemi için temizlik işlemi
-        os.system("cls")
-    else:  # Diğer işletim sistemleri için
+    if os.name == "nt":  # Windows işletim sistemini algıla
+        os.system("cls") # Windows'un ekran temizleme komutu
+    else:  # Diğer işletim sistemleri için (Linux/MacOS)
         os.system("clear")
-    
-    # Web pinger başlığını ekrana yazdırma
+
     web_pinger_art()
     
-    # Kullanıcıya seçeneklerin listesini gösterme
     sys.stdout.write(Fore.LIGHTCYAN_EX + "Seçenekler:" + "\n")
     sys.stdout.write(Fore.LIGHTYELLOW_EX + "[1] " + Fore.LIGHTCYAN_EX + "Belirli bir URL'ye sürekli ping gönder" + "\n")
     sys.stdout.write(Fore.LIGHTYELLOW_EX + "[2] " + Fore.LIGHTCYAN_EX + "Girilen DNS'nin ana adresini çöz" + "\n")
@@ -59,65 +63,56 @@ def main():
 
 # Belirli bir URL'ye sürekli ping gönderen fonksiyon
 def ping_continuous():
-    # Windows veya diğer işletim sistemleri için temizlik işlemi
+    # Windows veya diğer işletim sistemleri için ekran temizleme işlemleri bölümü
     if os.name == "nt":
         os.system("cls")
     else:
         os.system("clear")
-    
-    # Web pinger başlığını ekrana yazdırma
+
     web_pinger_art()
-    
-    # Kullanıcıdan ping atılacak URL'yi al
-    url = input("Lütfen ping atmak istediğiniz URL'yi girin: ")
+    url = input("Lütfen ping atmak istediğiniz URL'yi girin: ") # Kullanıcıdan ping atılacak URL'yi al
     
     try:
         while True:
-            # Ping işlemini gerçekleştirme
-            process = subprocess.Popen(["ping", "-t", url], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+            process = subprocess.Popen(["ping", "-t", url], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True) # Ping işlemini gerçekleştirir
 
             try:
-                # Ping sonuçlarını alarak işlemi bekletme
                 for line in process.stdout:
                     print(line.strip())
-            except KeyboardInterrupt:
-                # Ctrl+C'ye basıldığında ping işlemini durdur
-                print("\nPing gönderme işlemi durduruldu.")
+            except KeyboardInterrupt: # Ctrl+C kısayolunu dinler
+                print("\nPing gönderme işlemi durduruldu.") # Ctrl+C'ye basıldığında ping işlemini durdur
                 break
             finally:
-                # İşlem tamamlandığında process objesini sonlandır
+                # İşlem tamamlandığında "process" yani "işlem" mnanasına gelen nesneyi sonlandır
                 process.terminate()
                 process.wait()
                 
     except Exception as e:
         print("Bir hata oluştu:", e)
 
-# Girilen URL'nin IP adresini çözen fonksiyon
-def resolve_address():
-    # Windows veya diğer işletim sistemleri için temizlik işlemi
+def resolve_address(): # Girilen URL'nin IP adresini çözen fonksiyon
+    # Windows veya diğer işletim sistemleri için ekran temizleme işlemleri bölümü
     if os.name == "nt":
         os.system("cls")
     else:
         os.system("clear")
     
-    # Web pinger başlığını ekrana yazdırma
     web_pinger_art()
     
-    # Kullanıcıdan çözümlenmek istenen URL'yi al
-    address = input(Fore.LIGHTYELLOW_EX + " [>] " + Fore.LIGHTCYAN_EX + "Lütfen çözümlemek istediğiniz URL'yi girin: ")
+    address = input(Fore.LIGHTYELLOW_EX + " [>] " + Fore.LIGHTCYAN_EX + "Lütfen çözümlemek istediğiniz URL'yi girin: ") # Kullanıcıdan çözümlenmek istenen URL'yi al
     
     try:
         # URL'nin IP adresini çözme işlemi
         resolved_addresses = socket.getaddrinfo(address, None)
         
-        # Çözümlenen IP adreslerini ekrana yazdırma
+        # Çözümlenen IP adreslerini ekrana yazdırma kısmı
         print(Fore.LIGHTCYAN_EX + f"{address} adresinin çözümlenmiş IP adresleri:")
         for item in resolved_addresses:
-            print(Fore.LIGHTMAGENTA_EX + item[4][0])
-            input("\n" + Fore.LIGHTYELLOW_EX + " [?] " + "Enter'a basarak tekrar ana menüye geçiş yapın.")
+            print(Fore.LIGHTMAGENTA_EX + item[4][0]) # Çözümlenen IP adreslerini ekrana yazdır
+            input("\n" + Fore.LIGHTYELLOW_EX + " [?] " + "Enter'a basarak tekrar ana menüye geçiş yapın.")  # Enter yerine başka tuşları da tıklasa her türlü geçer
     except Exception as e:
         print("Bir hata oluştu:", e)
 
 # Ana program bölümü
 if __name__ == "__main__":
-    main()  # Ana döngüyü başlatma
+    main()  # Ana döngüyü başlatır
