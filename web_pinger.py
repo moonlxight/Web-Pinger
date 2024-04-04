dd9e3b4c35930914bb8ab2853cddd97dc0c9bcbb
moonlxight <furkandincer444@gmail.com>
2024-04-04 16:22:02 +0300

    Update web_pinger.py

--- web_pinger.py
+++ web_pinger.py
@@ -26,14 +26,21 @@ while True:
     choice = input("Lütfen seçim yapınız: ")
 
     if choice == "1":
-    url = input("Lütfen ping atmak istediğiniz URL'yi girin: ")
-    count = int(input("Kaç ping göndermek istiyorsunuz?: "))
+        url = input("Lütfen ping atmak istediğiniz URL'yi girin: ")
+        count = int(input("Kaç ping göndermek istiyorsunuz?: "))
+        success, result = ping_url(url, count)
+        if success:
+            print(Fore.LIGHTGREEN_EX + f"{url} adresine ping başarıyla gönderildi.")
+            print("Ping sonucu:", result)
+        else:
+            print(Fore.RED + f"{url} adresine ping gönderilirken bir hata meydana geldi!")
+            print("Hata mesajı:", result)
+        print(Style.RESET_ALL)
+    elif choice == "2":
+        print("Programdan çıkılıyor...")
+        break
+    else:
+        print(Fore.LIGHTYELLOW_EX + "Böyle bir seçenek yok.")
+        print(Style.RESET_ALL)
 
 success, result = ping_url(url)
-
-if success:
-    print(f"{url} adresine ping başarıyla gönderildi.")
-    print("Ping sonucu:", result)
-else:
-    print(f"{url} adresine ping gönderilirken bir hata oluştu.")
-    print("Hata mesajı:", result)
